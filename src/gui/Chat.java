c871b01bb10c7c8a7b98f74ae4fe49685c2af769
pablocs14 <pablocsanchez88@gmail.com>
2024-06-11 23:50:45 +0200

    commit 2

--- src/gui/Chat.java
+++ src/gui/Chat.java
@@ -19,7 +19,7 @@ public class Chat extends JFrame {
     private InetAddress serverAddress;
     private int serverPort = 12345;
 
-    public Chat(String nickname) {//asdasdasdasdasdas
+    public Chat(String nickname) {
         Chat.nickname = nickname;
         nicknameLabel.setText(nickname);
 
